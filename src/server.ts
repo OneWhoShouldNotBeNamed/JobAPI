830308e6bae4423c6355c42f53c52de7d1640e1f
Mahesh <maheshsk019@gmail.com>
2025-05-13 13:37:21 +0530

    updated server.ts

--- src/server.ts
+++ src/server.ts
@@ -8,5 +8,8 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI!;
 
 mongoose.connect(MONGO_URI)
-  .then(() => console.log('✅ Connected to MongoDB Atlas'))
+  .then(() =>{ console.log('✅ Connected to MongoDB Atlas'),
+    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+  })
   .catch((err) => console.error('❌ MongoDB connection error:', err));
